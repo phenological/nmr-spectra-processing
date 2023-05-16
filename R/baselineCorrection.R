0407033261889c1115a3557cf02cbc90ff38f837
Andres Bernal <otrobernal@gmail.com>
2023-05-16 12:04:54 +0800

    optional arguments

--- R/baselineCorrection.R
+++ R/baselineCorrection.R
@@ -1,8 +1,10 @@
 #' Baseline Correction
 #' @param Y matrix, series in rows
+#' @param ... other arguments to ptw::asysm
 #' @returns a matrix of same size with each row being corrected
 #' @importFrom ptw baseline.corr
-baselineCorrection <- function(Y) {
-  bc <- ptw::baseline.corr(Y)
+#' @details ptw:baseline.corr wrapper. Estimates baseline by asymmetric least squares using ptw::asysm an subtracts it from the spectra
+baselineCorrection <- function(Y,...) {
+  bc <- ptw::baseline.corr(Y,...)
   return(bc)
 }
