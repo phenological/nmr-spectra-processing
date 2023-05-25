84e49dc642b19781cd68ca31f1833899afabaff3
Andres Bernal <otrobernal@gmail.com>
2023-05-25 16:27:40 +0800

    Added wrapper to overlay spectra with graphics::matplot, calibrateSpectra can work on a custom reference singlet or doublet

--- R/calibrateSpectra.R
+++ R/calibrateSpectra.R
@@ -2,14 +2,14 @@
 #' Calibrates spectra
 #' @param x numeric, spectra scale e.g. ppm
 #' @param Y matrix, intensities, spectra in rows
-#' @param ref character, reference signal for calibration. Supported references: 'glucose', 'alanine', 'tsp'
+#' @param ref character, semi-optional. Reference signal for calibration. Supported references: 'glucose', 'alanine', 'tsp'. One of ref and cshift must be provided.
 #' @param rOref numeric, optional. Limits of the Region of
 #' Reference withing which the reference signal for calibration will be aligned.
 #' Defaults: 5.15 - 5.3 for glucose, 1.4 - 1.56 for alanine, and -0.2 - 0.2 for tsp.
 #' @param cshift numeric, chemical shift where the reference signal should be in the output
 #' Defaults to the center of the rOref
 #' @param j numeric. For doublet references, the coupling constant of the doublet
-#' Defaults: .0065 for glucose and .0125 for alanine
+#' Defaults: .0065 for glucose and .0125 for alanine. 
 #' @param threshold numeric, minimum cross-correlation requiered for alignment
 #' see alignSeries
 #' @param ... additional arguments for alignSeries (see details)
@@ -23,21 +23,25 @@ calibrateSpectra <- function(x, Y, ref, rOref, cshift, j, maxShift=1/3, threshol
   #standards for refs
   rOrefs <- list(glucose=c(5.15,5.3)
                  ,alanine=c(1.4,1.56)
-                 ,tsp=c(-0.2,0.2)
+                 ,tsp=c(-0.1,0.1)
                  )
   js <- list(glucose=0.0065,alanine=0.0125)
   
   #qc ref
   if (missing(ref)){
-    cat(crayon::red("calibrateSpectra >>","ref must be"
-                    ,"either 'glucose','alanine' or 'tsp'"))
-    stop()
+    if (missing(rOref)){
+      cat(crayon::red("calibrateSpectra >>","you must provide a reference signal"
+                      ,"for calibration; either a standard"
+                      ,"ref=c('glucose','alanine', 'tsp'), or a custom."
+                      ,"reference via rOref, cshift and j\n"))
+      stop()
+    }
   }
   else{
     if (!(ref %in% c("alanine","glucose","tsp"))){
-      cat(crayon::red("calibrateSpectra >>","ref must be"
-                      ,"either 'glucose','alanine' or 'tsp'"))
-      stop()
+    cat(crayon::red("calibrateSpectra >>","supported ref are:"
+                    ,"'glucose','alanine' or 'tsp'\n"))
+    stop()
     }
   }
   
@@ -57,15 +61,25 @@ calibrateSpectra <- function(x, Y, ref, rOref, cshift, j, maxShift=1/3, threshol
   if (missing(cshift)) cshift <- mean(rOref)
   else{
     if (!is.numeric(cshift) | (cshift < min(x[rOref])) | (cshift > max(x[rOref]))){
-      cat(crayon::red("calibrateSpectra >>", "Invalid cshift value"))
+      cat(crayon::red("calibrateSpectra >>", "Invalid cshift value\n"))
+      stop()
     }
   }
   
   #qc j
-  if (missing(j)) j <- js[[ref]]
+  if (missing(j)){
+    if (missing(ref)){
+      j=NULL
+    }
+    else{
+      if (ref %in% c("alanine","glucose")) j <- js[[ref]]
+      else j=NULL
+    }
+  }
   else{
-    if (!is.numeric(j)){
-      cat(crayon::red("calibrateSpectra >>", "Invalid coupling constant j"))
+    if (!is.numeric(j) | length(j)!=1){
+      cat(crayon::red("calibrateSpectra >>", "Invalid coupling constant j\n"))
+      stop()
     }
   }
   
@@ -73,8 +87,8 @@ calibrateSpectra <- function(x, Y, ref, rOref, cshift, j, maxShift=1/3, threshol
   rOref <- x >= rOref[1] & x <= rOref[2]
   
   #generate model reference peak
-  #TBC: is it ok to always model with a lorentzian?
-  if (ref=="tps") means <- cshift
+  #If no j, model a singulet
+  if (is.null(j)) means <- cshift
   else means <- c(cshift - j/2, cshift + j/2)
   ref <- rowSums(lorentzian(x[rOref],mean=means
                    ,fwhm=0.001))
---
+++ R/smatplot.R
@@ -0,0 +1,25 @@
+#' Fast spectra overlay plot
+#' 
+#' Wrapper to matplot for plotting spectra. Crude compared to ggplot2 but a lot faster.
+#' @param x numeric, your x scale e.g. ppm
+#' @param Y matrix, spectra matrix, one spectrum per row
+#' @param limits, numeric, optional, limits of the region to be plotted. Defaults to full spectra.
+#' @param by, numeric, optional, number of spectra to be overlay on each plot
+#' @param type, optional, defaults to "l" (lines), default strongly recommended
+#' @param ..., additional options to be passed to matplot
+#' @returns NULL
+#' @export
+smatplot <- function(x, Y, by, limits, type="l",...){
+  if (!missing(limits)){
+    fi <- x >= limits[1] & x <= limits[2]
+    x <- x[fi]
+    Y <- Y[,fi]
+  }
+  if (missing(by)) matplot(x,t(Y),type=type,...)
+  else{
+    n <- floor(dim(Y)[1] / by)
+    r <- dim(Y)[1] %% by
+    for (j in 1:n) matplot(x,t(Y[1:by + by*(j-1),]),type=type,...)
+    if (r>0) matplot(x,t(Y[(by*n+1):(by*n+r),]),type=type,...)
+  }
+}
