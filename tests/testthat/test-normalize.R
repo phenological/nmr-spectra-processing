509402a0fb45398ebeeea794c7ba421324a3ab11
julien wist <julien wist@correounivalle.edu.co>
2024-04-04 14:26:16 +0800

    udpate and release 1.1.0

--- tests/testthat/test-normalize.R
+++ tests/testthat/test-normalize.R
@@ -1,5 +1,5 @@
 test_that("normalize works", {
-  expect_equal(unique(rowSums(normalize(avo3,sum))),1)
+  expect_equal(unique(round(rowSums(normalize(avo3,sum)),3)),1)
   expect_equal(unique(max(normalize(avo3,max))),1)
   expect_equal(round(pqn(avo3),3),c(1.000,1.017,0.896))
 })
